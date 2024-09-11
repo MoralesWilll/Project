ad8394116bdc0ae34db7dd97191f88a50b5a4ad4
ecc97 <carmonaedwin1123@gmail.com>
2024-09-11 13:17:38 -0400

    feat: add show password on the input password and loader on the button to login

--- front/src/components/login/LoginForm.tsx
+++ front/src/components/login/LoginForm.tsx
@@ -5,10 +5,13 @@ import { useState } from "react"
 import Form from "@/components/form/Form"
 import Label from "@/components/ui/Label.ui"
 import Input from "@/components/ui/Input.ui"
+import InputPassword from "@/components/ui/InputPassword.ui"
 import Button from "@/components/ui/Button.ui"
+import Loader from "@/components/ui/Loader.ui"
 import { Container, ContainerForm, InputContent, TextError, BackgroundForm, GroupTitle } from "@/components/form/styledForm"
 import Link from "next/link"
 import { colors } from "@/app/GlobalStyles"
+import { FaEye, FaEyeSlash } from "react-icons/fa"
 
 interface UserLogin {
     username: string
@@ -24,6 +27,8 @@ const initialState: UserLogin = {
 const Login: React.FC = () => {
     const [user, setUser] = useState<UserLogin>(initialState)
     const [error, setError] = useState<string>("")
+    const [showPassword, setShowPassword] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setUser({ ...user, [e.target.name]: e.target.value })
@@ -32,6 +37,8 @@ const Login: React.FC = () => {
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         setError("")
+        setIsLoading(true)
+
         try {
             const response = await fetch(`http://localhost:4000/users?username=${user.username}`)
 
@@ -53,6 +60,8 @@ const Login: React.FC = () => {
         } catch (error) {
             console.error('Error:', error)
             setError('Error')
+        } finally {
+            setIsLoading(false)
         }
         console.log(user)
         setUser(initialState)
@@ -81,8 +90,8 @@ const Login: React.FC = () => {
                     </InputContent>
                     <InputContent>
                         <Label label="Contraseña" for="password" />
-                        <Input
-                            type="password"
+                        <InputPassword
+                            type={showPassword ? "text" : "password"}
                             id="password"
                             name="password"
                             placeholder="Contraseña"
@@ -90,10 +99,21 @@ const Login: React.FC = () => {
                             onChange={handleChange}
                             $padding="1rem"
                             required
+                            icon={
+                                <span onClick={() => setShowPassword(!showPassword)}>
+                                    {showPassword ? <FaEyeSlash color={colors.white} /> : <FaEye color={colors.white} />}
+                                </span>
+                            }
                         />
                     </InputContent>
                     {error && <TextError>{error}</TextError>}
-                    <Button type="submit" disabled={user.username === "" || user.password === ""} $bgColor={colors.white}>Ingresar</Button>
+                    <Button type="submit" disabled={user.username === "" || user.password === ""} $bgColor={colors.white}>
+                        {isLoading ? (
+                            <Loader />
+                        ): (
+                            <span>Ingresar</span>
+                        )}
+                    </Button>
                     <p>¿No tienes una cuenta? <Link href="/pages/register">Registrarse</Link></p>
                 </Form>
             </ContainerForm>
