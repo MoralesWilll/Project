d0cf1645a90663f4464ffb950b40ddf222bc1f41
MoralesWilll <will1ams.moralesm22@gmail.com>
2024-09-10 20:50:57 -0500

    feat: implement user registration endpoint

---
+++ back/src/main/java/Contest/Project/services/UserService.java
@@ -0,0 +1,37 @@
+package Contest.Project.services;
+
+import Contest.Project.dtos.UserDTO;
+import Contest.Project.entities.User;
+import Contest.Project.repositories.RoleRepository;
+import Contest.Project.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public User register(UserDTO userDTO) {
+        if (userRepository.findByEmail(userDTO.getEmail()) != null) {
+            throw new RuntimeException("Email is already in use");
+        }
+
+        User user = new User();
+        user.setUserName(userDTO.getUserName());
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+        Integer roleId = userDTO.getIdRole();
+        user.setIdRole(roleRepository.findById(roleId).orElse(null));
+        return userRepository.save(user);
+    }
+
+}
