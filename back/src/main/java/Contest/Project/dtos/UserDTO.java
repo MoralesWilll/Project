e4a7350264d2562a58c34094022f202cca7ac0ee
MoralesWilll <163943057+MoralesWilll@users.noreply.github.com>
2024-09-18 18:10:00 -0500

    Feature/properties endpoints (#10)
    * feat: Entity property endpoint completed
    
    * fix: CRUD fix for better coding
    
    * fix: CRUD fix for better coding
    
    * fix: CRUD fix for better coding
    
    * fix: CRUD fix for better coding
    
    * fix: add missing validations and DTOs
    
    * fix: fix property creation endpoint
    
    * feat: add missing interfaces for classes
    
    * fix: add missing atribute to DTO
    
    * refactor: change the name of the interface to match conventions
    
    * feat: add DTO to filter properties
    
    * feat: add properties filter logic and endpoint
    
    ---------
    Co-authored-by: BrayanConde23 <soycondebrayan@gmail.com>

--- back/src/main/java/Contest/Project/dtos/UserDTO.java
+++ back/src/main/java/Contest/Project/dtos/UserDTO.java
@@ -2,6 +2,7 @@ package Contest.Project.dtos;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -19,5 +20,6 @@ public class UserDTO {
     @NotBlank(message = "Password cannot be blank")
     private String password;
 
+    @NotNull
     private int idRole;
 }
