788db0c222058ebd4c0aae339992cea44e2ac623
MoralesWilll <will1ams.moralesm22@gmail.com>
2024-09-18 17:58:15 -0500

    refactor: change the name of the interface to match conventions

--- back/src/main/java/Contest/Project/repositories/PropertiesRepository.java
+++
@@ -1,7 +0,0 @@
-package Contest.Project.repositories;
-
-import Contest.Project.entities.Property;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PropertiesRepository extends JpaRepository<Property, Integer> {
-}
